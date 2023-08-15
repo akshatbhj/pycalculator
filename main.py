0b749037d5436364f532efe7fe433634127078ab
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2023-08-15 21:39:02 +0530

    Changing Background

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+
+BLACK_BG = "#17161b"
+
+root = Tk()
+root.title("Pycalculator")
+root.geometry("570x600")
+root.resizable(False,False)
+root.config(background=BLACK_BG,)
+
+root.mainloop()
