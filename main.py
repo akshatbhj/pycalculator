820ce6a537bf9446187a59ad784d99cafe628e75
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2023-08-15 21:53:49 +0530

    Adding First Row of Buttons

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 
 BLACK_BG = "#17161b"
 BUTTON_BG = "#3697f5"
+BUTTON_BG_2 = "#2a2d36"
 WHITE = "#ffffff"
 DEFAULT_FONT = "Arial"
 
@@ -27,5 +28,38 @@ Button(
     bg=BUTTON_BG,
     fg=WHITE,
 ).place(x=10, y=100)
+
+Button(
+    root,
+    text="/",
+    width=5,
+    height=1,
+    font=(DEFAULT_FONT, 30, "bold"),
+    bd=1,
+    bg=BUTTON_BG_2,
+    fg=WHITE,
+).place(x=150, y=100)
+
+Button(
+    root,
+    text="%",
+    width=5,
+    height=1,
+    font=(DEFAULT_FONT, 30, "bold"),
+    bd=1,
+    bg=BUTTON_BG_2,
+    fg=WHITE,
+).place(x=290, y=100)
+
+Button(
+    root,
+    text="*",
+    width=5,
+    height=1,
+    font=(DEFAULT_FONT, 30, "bold"),
+    bd=1,
+    bg=BUTTON_BG_2,
+    fg=WHITE,
+).place(x=430, y=100)
 
 root.mainloop()
